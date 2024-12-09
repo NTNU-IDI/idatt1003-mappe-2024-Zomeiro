9fd1b726de4221825d6a912d96c631aec20dada4
Zomeiro <ba.vangen5@gmail.com>
2024-12-10 00:16:41 +0100

    toString method modifications

--- Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/Unit.java
+++ Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/Unit.java
@@ -1,6 +1,33 @@
 package edu.ntnu.idatt;
 
+/**
+ * Enum representing units of measurement for groceries. Currently supports "gram" and "litre".
+ */
 public enum Unit {
+  /**
+   * Unit representing grams.
+   */
   GRAM,
-  LITRE
+
+  /**
+   * Unit representing litres.
+   */
+  LITRE;
+
+  /**
+   * Returns a string representation of the unit. Overrides the default {@code toString()} method to
+   * provide a lowercase version of the unit name.
+   *
+   * @return A string representation of the unit ("gram" or "litre").
+   */
+  @Override
+  public String toString() {
+    if (this == GRAM) {
+      return "gram";
+    } else if (this == LITRE) {
+      return "litre";
+    } else {
+      return super.toString();
+    }
+  }
 }
