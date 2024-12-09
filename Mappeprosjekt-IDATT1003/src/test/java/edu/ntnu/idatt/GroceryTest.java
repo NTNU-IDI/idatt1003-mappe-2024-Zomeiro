9859ea6824fd8b1d2e9fcdfa9efbba8efc4fe13e
Zomeiro <ba.vangen5@gmail.com>
2024-12-10 00:15:44 +0100

    Tests the Grocery class methods

---
+++ Mappeprosjekt-IDATT1003/src/test/java/edu/ntnu/idatt/GroceryTest.java
@@ -0,0 +1,80 @@
+package edu.ntnu.idatt;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class GroceryTest {
+
+  String name;
+  double amount;
+  Unit unit;
+  LocalDate expiryDate;
+  double unitPrice;
+
+  Grocery testObject;
+
+  @BeforeEach
+  void createTestObject() {
+    name = "Milk";
+    amount = 1.75;
+    unit = Unit.LITRE;
+    expiryDate = LocalDate.of(2024, 12, 23);
+    unitPrice = 18.20;
+
+    testObject = new Grocery(name, amount, unit, expiryDate, unitPrice);
+  }
+
+  @Test
+  void getName() {
+    assertEquals(name, testObject.getName());
+  }
+
+  @Test
+  void getAmount() {
+    assertEquals(amount, testObject.getAmount());
+  }
+
+  @Test
+  void getUnit() {
+    assertEquals(unit, testObject.getUnit());
+  }
+
+  @Test
+  void getExpiryDate() {
+    assertEquals(expiryDate, testObject.getExpiryDate());
+  }
+
+  @Test
+  void getUnitPrice() {
+    assertEquals(unitPrice, testObject.getUnitPrice());
+  }
+
+  @Test
+  void getTotalPrice() {
+    assertEquals(amount * unitPrice, testObject.getTotalPrice());
+  }
+
+  @Test
+  void setAmount() {
+    testObject.setAmount(1);
+    assertEquals(1, testObject.getAmount());
+  }
+
+  @Test
+  void setExpiryDate() {
+    testObject.setExpiryDate(LocalDate.of(2024, 12, 31));
+    assertEquals(LocalDate.of(2024, 12, 31), testObject.getExpiryDate());
+  }
+
+  @Test
+  void testToString() {
+    String groceryToString =
+        name + ":\nAmount: " + amount + "\nUnit: " + unit.toString() + "\nExpiryDate: "
+            + expiryDate.toString();
+    assertEquals(groceryToString, testObject.toString());
+  }
+}
