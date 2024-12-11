9dcfa0a4475bcfb7f277a810f650db2e4df3a369
zomeiro <ba.vangen5@gmail.com>
2024-12-11 20:56:17 +0100

    created recipe class

---
+++ Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/Recipe.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idatt;
+
+import java.util.List;
+
+public class Recipe {
+    private final String description;
+    private final String instructions;
+    private List<Grocery> ingredients;
+    private double portions;
+
+    public Recipe(String description, String instructions, List<Grocery> ingredients, double portions) {
+        this.description = description;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+        this.portions = portions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+    public List<Grocery> getIngredients() {
+        return ingredients;
+    }
+    public double getPortions() {
+        return portions;
+    }
+
+    void setPortions(double newPortions) {
+        double portionRatio = newPortions/portions;
+        for (Grocery ingredient : getIngredients()) {
+            ingredient.setAmount(ingredient.getAmount()*portionRatio);
+        }
+    }
+}
