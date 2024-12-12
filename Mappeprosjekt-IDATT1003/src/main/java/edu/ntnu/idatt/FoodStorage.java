c922c9d52027340c4887fcf08cc15e31e5b197a5
Zomeiro <ba.vangen5@gmail.com>
2024-12-12 07:51:38 +0100

    Added new exceptions to removeAmount and improved structure

--- Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/FoodStorage.java
+++ Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/FoodStorage.java
@@ -2,7 +2,6 @@ package edu.ntnu.idatt;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -33,35 +32,42 @@ public class FoodStorage {
     if (grocery == null) {
       throw new IllegalArgumentException("grocery cannot be null");
     }
-    groceries.putIfAbsent(grocery.getName(), new ArrayList<Grocery>());
+    groceries.putIfAbsent(grocery.getName(), new ArrayList<>());
     groceries.get(grocery.getName()).add(grocery);
   }
 
   /**
    * Removes a specified amount of a grocery item, starting with the oldest.
    *
-   * @param Key           the name of the grocery to remove
+   * @param groceryName   the name of the grocery to remove
    * @param amountRemoved the amount to remove
    */
-  public void removeAmount(String Key, double amountRemoved) {
-
-    if (groceries.isEmpty()) {
-      System.out.println("No groceries found");
-    } else if (!groceries.containsKey(Key)) {
-      System.out.println("No grocery with that key found");
-    } else {
-      groceries.get(Key).sort(Comparator.comparing(Grocery::getExpiryDate));
-      while (amountRemoved != 0) {
-        if (groceries.get(Key).getFirst().getAmount() > amountRemoved) {
-          groceries.get(Key).getFirst()
-              .setAmount(groceries.get(Key).getFirst().getAmount() - amountRemoved);
-          amountRemoved = 0;
-        } else {
-          amountRemoved -= groceries.get(Key).getFirst().getAmount();
-          groceries.get(Key).removeFirst();
-        }
+  public void removeAmount(String groceryName, double amountRemoved) {
+    /* tolerance set to 1g/1ml in order to circumvent floating point inaccuracies
+    when trying to remove the entire amount */
+    double tolerance = 0.001;
+    if (groceryName == null || groceryName.trim().isEmpty()) {
+      throw new IllegalArgumentException("Grocery name cannot be null or empty");
+    }
+    if (!groceries.containsKey(groceryName)) {
+      throw new IllegalArgumentException("No grocery found with the name: " + groceryName);
+    }
+    if (getTotalAmount(groceryName) < amountRemoved + tolerance) {
+      throw new IllegalArgumentException("Amount to remove cannot be larger than the total amount");
+    }
+    
+    groceries.get(groceryName).sort(Comparator.comparing(Grocery::getExpiryDate));
+    while (amountRemoved > tolerance) {
+      if (groceries.get(groceryName).getFirst().getAmount() > amountRemoved) {
+        groceries.get(groceryName).getFirst()
+            .setAmount(groceries.get(groceryName).getFirst().getAmount() - amountRemoved);
+        amountRemoved = 0;
+      } else {
+        amountRemoved -= groceries.get(groceryName).getFirst().getAmount();
+        groceries.get(groceryName).removeFirst();
       }
     }
+
   }
 
   /**
@@ -123,12 +129,8 @@ public class FoodStorage {
     if (!groceries.containsKey(groceryName)) {
       throw new IllegalArgumentException("No grocery found with the name: " + groceryName);
     }
-    if (groceries.containsKey(groceryName)) {
-      for (Grocery g : groceries.get(groceryName)) {
-        System.out.println(g.toString());
-      }
-    } else {
-      System.out.println("No grocery with name " + groceryName + " found");
+    for (Grocery g : groceries.get(groceryName)) {
+      System.out.println(g.toString());
     }
   }
 
@@ -139,13 +141,12 @@ public class FoodStorage {
     if (groceries.isEmpty()) {
       System.out.println("No groceries found");
     } else {
-        groceries.keySet().stream()
-            .sorted()
-            .forEach(key -> {
-              System.out.println(key + ":");
-              groceries.get(key).forEach(System.out::println);
-            });
-      }
+      groceries.keySet().stream()
+          .sorted()
+          .forEach(key -> {
+            System.out.println(key + ":");
+            groceries.get(key).forEach(System.out::println);
+          });
     }
   }
 }
