d4a2827f80944d9209ffc719009a7bd0c5bfaab9
Zomeiro <ba.vangen5@gmail.com>
2024-12-12 04:52:23 +0100

    reformat

--- Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/CookBook.java
+++ Mappeprosjekt-IDATT1003/src/main/java/edu/ntnu/idatt/CookBook.java
@@ -1,20 +1,21 @@
 package edu.ntnu.idatt;
+
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CookBook {
-    private HashMap<String,Recipe> recipes;
-    FoodStorage foodStorage;
-    public CookBook(FoodStorage foodStorage) {
-        this.foodStorage = foodStorage;
-    }
 
-    void createRecipe(String name, String description, String instructions, List<Grocery> groceryList, double portions) {
-        recipes.put(name, new Recipe(description, instructions, groceryList, portions));
-    }
+  FoodStorage foodStorage;
+  private HashMap<String, Recipe> recipes;
 
-    public Boolean checkRecipeAvailability(String recipeName) {
+  public CookBook(FoodStorage foodStorage) {
+    this.foodStorage = foodStorage;
+  }
 
-    }
+  void createRecipe(String name, String description, String instructions, List<Grocery> groceryList,
+      double portions) {
+    recipes.put(name, new Recipe(description, instructions, groceryList, portions));
+  }
+
+
 }
